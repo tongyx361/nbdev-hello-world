cd6dbc45339c8b152340aa55e588ba1468a09f24
Shawn Yuxuan Tong <tongyuxuan361@gmail.com>
2024-02-05 01:20:23 +0800

    Add `say_hello`; update index

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://tongyx361.github.io',
                 'git_url': 'https://github.com/tongyx361/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')},
+            'nbdev_hello_world.test': { 'nbdev_hello_world.test.foo': ('test.html#foo', 'nbdev_hello_world/test.py'),
+                                        'nbdev_hello_world.test.say_hello': ('test.html#say_hello', 'nbdev_hello_world/test.py')}}}
